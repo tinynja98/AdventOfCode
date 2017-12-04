49ae6c56ceeebf5c447e5def7ba833b50046fea8
tinynja98 <minou-98-@hotmail.com>
2017-12-04 05:08:15 -0500

    fixed the exit command not exiting

--- 2015/main.lua
+++ 2015/main.lua
@@ -13,27 +13,27 @@ pwd = string.upper(io.popen("echo %cd%"):read('*l').."\\")
 local pwdComplement = string.gsub(string.upper(debug.getinfo(1).source:sub(2)),pwd,"")
 pwd = string.gsub(pwd..string.sub(pwdComplement,0,string.find(pwdComplement,"MAIN.LUA")-1),"\\","/")
 _dofile(string.sub(pwd,1,string.find(pwd,"2015")-1).."/utils.lua")
-local dayList,args,input = {},{},""
+local dayList,inputs,read = {},{},""
 
 repeat
   repeat
     io.write("What day do you want to solve? ")
-    input = io.read()
+    read = io.read()
     for fileName in io.popen([[dir "]]..pwd..[[" /B /AD]]):lines() do
       if string.match(fileName,"Day [0-9]+") then
         table.insert(dayList, fileName)
       end
     end
-    args = string.cut(input," ")
-  until tonumber(args[1]) ~= nil or args[1] == "exit"
-  if args[1] ~= "exit" then
-    local dayFolder = "Day "..args[1]
+    inputs = string.cut(read," ")
+  until tonumber(inputs[1]) ~= nil or inputs[1] == "exit"
+  if inputs[1] ~= "exit" then
+    local dayFolder = "Day "..inputs[1]
     if table.find(dayList,dayFolder) then
       io.write("\n")
-      xpcall(loadfile(dayFolder.."/main.lua"),print,table.unpack(args,2))
+      xpcall(loadfile(dayFolder.."/main.lua"),print,table.unpack(inputs,2))
     else
       print("That day has not yet been solved.")
     end
     io.write("\n")
   end
-until day == "exit"
+until inputs[1] == "exit"
